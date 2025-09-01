d9cc3af8f49a8a93f006d5c33da4c4fdcfe27e1d
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2025-09-01 16:52:32 +0100

    update

--- routes/performance.js
+++ routes/performance.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-const express = require('express');
-const router = express.Router();
-const db = require('../db');
-
 router.get('/ca/vie', async (req, res) => {
   try {
     const result = await db.query(`
