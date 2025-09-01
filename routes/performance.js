df4b5f61b6ca7fb12741900871be6479d63531cf
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2025-09-01 16:24:33 +0100

    update

--- routes/performance.js
+++ routes/performance.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.get('/ca', async (req, res) => {
+router.get('/performance/ca/vie', async (req, res) => {
   try {
     const result = await db.query(`
       SELECT
@@ -17,6 +17,25 @@ router.get('/ca', async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Erreur serveur');
+  }
+});
+
+router.get('/performance/ca/auto', async (req, res) => {
+  try {
+    const result = await db.query(`
+      SELECT
+        SUM(CASE WHEN date_creation_client::date = CURRENT_DATE THEN prime ELSE 0 END) AS ca_aujourdhui,
+        SUM(CASE WHEN date_creation_client >= date_trunc('week', CURRENT_DATE) THEN prime ELSE 0 END) AS ca_semaine,
+        SUM(CASE WHEN date_creation_client >= date_trunc('month', CURRENT_DATE) THEN prime ELSE 0 END) AS ca_mois,
+        SUM(CASE WHEN date_creation_client >= date_trunc('year', CURRENT_DATE) THEN prime ELSE 0 END) AS ca_annee
+      FROM souscription_auto
+      WHERE id_agent = 1;
+    `);
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erreur serveur' });
   }
 });
 
