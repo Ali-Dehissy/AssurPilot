b60252a139985a72da11aeabfcdf851ade77925c
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2025-09-01 15:37:21 +0100

    update

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
   const express = require('express');
   const cors = require('cors');
   const db = require('./db');
+  const app = express();
   const clientsRouter = require('./routes/clients');
   const loginRouter = require('./routes/login');
-  const app = express();
+  const performanceRouter = require('./routes/performance');
 
   app.use(cors());
   app.use(express.json());
@@ -14,6 +15,7 @@
 
   app.use('/api/clients', clientsRouter);
   app.use('/api', loginRouter);
+  app.use('/api/performance', performanceRouter);
 
   const PORT = process.env.PORT || 3001;
   app.listen(PORT, () => {
