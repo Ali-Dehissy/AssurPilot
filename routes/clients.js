3cfd4b4d0d7b6310dbf4f98788ed543a8261797a
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2025-09-03 10:45:57 +0100

    update

--- routes/clients.js
+++ routes/clients.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
+/*Vie*/
 router.post('/', async (req, res) => {
   const client = req.body;
 
@@ -34,6 +35,40 @@ router.post('/', async (req, res) => {
     console.error('Erreur PostgreSQL:', err);
     res.status(500).json({ error: 'Erreur serveur.' });
   }
+});
+
+/*Auto*/
+router.post('/', async (req, res) => {
+const client = req.body;
+
+
+const query = `
+INSERT INTO souscription_auto (
+nom, prenom, raison_sociale, cin, matricule_fiscale, adresse,
+matricule_auto, date_mise_en_circulation, valeur_venale, date_creation_client,
+numero_contrat, type_voiture, id_agent, prime
+) VALUES (
+$1, $2, $3, $4, $5, $6,
+$7, $8, $9, $10,
+$11, $12, $13, $14
+) RETURNING id;
+`;
+
+
+const values = [
+client.nom, client.prenom, client.raison_sociale, client.cin, client.matricule_fiscale, client.adresse,
+client.matricule_auto, client.date_mise_en_circulation, client.valeur_venale, client.date_creation_client || new Date().toISOString(),
+client.numero_contrat, client.type_voiture, client.id_agent, client.prime
+];
+
+
+try {
+const result = await db.query(query, values);
+res.status(201).json({ id: result.rows[0].id, message: 'Client auto ajouté avec succès.' });
+} catch (err) {
+console.error('Erreur PostgreSQL:', err);
+res.status(500).json({ error: 'Erreur serveur.' });
+}
 });
 
 
